04060751bc4acb47e950eeac80c6ed889ee8e536
Cristina-hub04 <ioja.cristinag@gmail.com>
2026-04-29 17:09:57 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bookstore.Endpoints;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// Map the endpoints from BookEndpoints
-app.MapBookEndpoints();
-
-app.Run();
