f0b443ad1f0bc4fce7966eba0e4ef21a40919704
Cristina-hub04 <ioja.cristinag@gmail.com>
2026-04-29 17:08:33 +0300

    Delete Book.cs

--- Book.cs
+++
@@ -1,9 +0,0 @@
-namespace Bookstore;
-
-public class Book
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = "";
-    public string Author { get; set; } = "";
-    public double Price { get; set; }
-}
