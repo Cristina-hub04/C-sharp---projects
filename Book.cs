894186481f899ffa3bdb75a88612b4b4555b9c06
Cristina-hub04 <ioja.cristinag@gmail.com>
2026-04-29 17:06:39 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,9 @@
+namespace Bookstore;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Author { get; set; } = "";
+    public double Price { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Bookstore.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Map the endpoints from BookEndpoints
+app.MapBookEndpoints();
+
+app.Run();
